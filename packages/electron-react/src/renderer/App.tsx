8819ceffe0ab2a15ba58706feea67bdc8cd38824
Ameen Rashad <ameenrashadv@gmail.com>
2020-06-26 11:48:24 +0530

    feat : added sync provider to access sync

--- packages/electron-react/src/renderer/App.tsx
+++ packages/electron-react/src/renderer/App.tsx
@@ -1,13 +1,33 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { hot } from "react-hot-loader/root";
 import Home from "./pages/Home";
-import sync from "./service/sync/firesync";
+import { ISyncConfig } from "./service/sync";
+import { fdb } from "./service/firestore/app";
+import { getDBDir } from "./utils";
+import { SyncProvider } from "./providers/SyncProvider";
+
+const syncConfig: ISyncConfig = {
+  dbpath: getDBDir("sqlite", "sync.sqlite3"),
+  collections: {
+    // collection name and , name of fields to be synced
+    customers: [
+      { name: "name", type: "string" },
+      { name: "phone", type: "string" },
+      { name: "address", type: "string" },
+    ],
+    products: [
+      { name: "name", type: "string" },
+      { name: "description", type: "string" },
+    ],
+  },
+};
 
 function App() {
-  useEffect(() => {
-    console.log("Sync : Observing tables ", sync.watchingCollections);
-  });
-  return <Home />;
+  return (
+    <SyncProvider config={syncConfig} fireDB={fdb}>
+      <Home />
+    </SyncProvider>
+  );
 }
 
 export default hot(App);
---
+++ packages/electron-react/src/renderer/providers/SyncProvider.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useContext } from "react";
+import { IFireDB, ISync, ISyncConfig, initSync } from "../service/sync";
+
+type ContextProps = {
+  sync: ISync | undefined;
+};
+
+const SyncContext = React.createContext<ContextProps>({ sync: undefined });
+
+function useSync() {
+  return useContext(SyncContext);
+}
+
+interface ISyncProvider {
+  config: ISyncConfig;
+  fireDB: IFireDB;
+  children: React.ReactNode;
+}
+
+const SyncProvider = ({ config, fireDB, children }: ISyncProvider) => {
+  const [sync, setSync] = React.useState<ISync | undefined>(undefined);
+
+  useEffect(() => {
+    //TODO : (test) if the config is changed, will it re-initialize
+    const newSyncObj = initSync(config, fireDB);
+    setSync(newSyncObj);
+    console.log("kkk Sync : Initializing ");
+  }, [config, fireDB]);
+
+  return (
+    <SyncContext.Provider
+      value={{
+        sync: sync,
+      }}
+    >
+      {children}
+    </SyncContext.Provider>
+  );
+};
+
+export { useSync, SyncProvider };
