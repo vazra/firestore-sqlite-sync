4d9d9f27f9bfece38cf525a7de630787da5d9e60
Ameen Rashad <ameenrashadv@gmail.com>
2020-07-05 13:52:38 +0530

    chore: configured lerna repo

--- packages/core/src/firestore/utils.ts
+++ packages/core/src/firestore/utils.ts
@@ -1,74 +1,65 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import { IDoc } from "../types";
-import { ISync } from "../firesync";
-import { CNAME_SETTINGS, DOC_NAME_LAST_UPDATED } from "../constants";
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import { IDoc } from '../types'
+import { ISync } from '../firesync'
+import { CNAME_SETTINGS, DOC_NAME_LAST_UPDATED } from '../constants'
 
-export const serverTime = firebase.firestore.FieldValue.serverTimestamp;
-export const Timestamp = firebase.firestore.Timestamp;
+export const serverTime = firebase.firestore.FieldValue.serverTimestamp
+export const Timestamp = firebase.firestore.Timestamp
 
-export type IFireDB = firebase.firestore.Firestore;
+export type IFireDB = firebase.firestore.Firestore
+
+console.log('Loading module from ', __filename.slice(__dirname.length + 1))
 
 // retrieve data from snapshot with 'id' as a property
-export const dataFromSnapshot = (
-  snapshot: firebase.firestore.DocumentData
-): IDoc | undefined => {
+export const dataFromSnapshot = (snapshot: firebase.firestore.DocumentData): IDoc | undefined => {
   if (!snapshot.exists) {
-    console.log(`404! : dataFromSnapshot(${snapshot.id}) : NOT FOUND!`);
-    return undefined;
+    console.log(`404! : dataFromSnapshot(${snapshot.id}) : NOT FOUND!`)
+    return undefined
   }
-  const data = snapshot.data() as any;
+  const data = snapshot.data() as any
 
   return {
     ...data,
     id: snapshot.id,
-  };
-};
+  }
+}
 
-export const updateWithSync = async (
-  sync: ISync,
-  collectionId: string,
-  docId: string,
-  data: IDoc
-) => {
+export const updateWithSync = async (sync: ISync, collectionId: string, docId: string, data: IDoc) => {
   try {
     await sync.firedb
       .collection(collectionId)
       .doc(docId)
-      .update({ ...data, [sync.config.updatedTimeKey || "ut"]: serverTime() });
+      .update({ ...data, [sync.config.updatedTimeKey || 'ut']: serverTime() })
 
     await sync.firedb
       .collection(CNAME_SETTINGS)
       .doc(DOC_NAME_LAST_UPDATED)
-      .update({ [collectionId]: serverTime() });
-    Promise.resolve();
+      .update({ [collectionId]: serverTime() })
+    Promise.resolve()
   } catch (err) {
-    console.log("Err in updateWithSync");
-    console.warn(err);
-    Promise.reject(err);
+    console.log('Err in updateWithSync')
+    console.warn(err)
+    Promise.reject(err)
   }
-};
+}
 
-export const insertWithSync = async (
-  sync: ISync,
-  collectionId: string,
-  data: IDoc
-) => {
+export const insertWithSync = async (sync: ISync, collectionId: string, data: IDoc) => {
   try {
     await sync.firedb.collection(collectionId).add({
-      [sync.config.updatedTimeKey || "ut"]: serverTime(),
+      [sync.config.updatedTimeKey || 'ut']: serverTime(),
       ...data,
-    });
+    })
 
     await sync.firedb
       .collection(CNAME_SETTINGS)
       .doc(DOC_NAME_LAST_UPDATED)
-      .update({ [collectionId]: serverTime() });
-    Promise.resolve();
+      .update({ [collectionId]: serverTime() })
+    Promise.resolve()
   } catch (err) {
-    console.warn(err);
-    Promise.reject(err);
+    console.warn(err)
+    Promise.reject(err)
   }
-};
+}
 
 // TODO : make firebase as peer dependency
