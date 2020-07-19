6ce6576782acfcc0183d2a78f2864fa4f0554116
Ameen Rashad <ameenrashadv@gmail.com>
2020-07-20 01:28:21 +0530

    fix: __dirname is not availabe in web env

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,3 @@
-const dirpath = __dirname.split('node_modules/')[1]
-console.log('Loading core module from ', dirpath + '/' + __filename.slice(__dirname.length + 1))
-
 // export { default } from "./firesync";
 export * from './firesync'
 export * from './types'
